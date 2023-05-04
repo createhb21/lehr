8796096987b8a9f93eb8fbcfe8cdefc04c3b9d16
createhb21 <alsghk9701@gmail.com>
2023-05-04 17:22:15 +0900

    ✨ 채용 리스트 페이지 태그 마크업 (#9)

--- src/components/common/list/List.component.tsx
+++ src/components/common/list/List.component.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 
 import * as S from './List.styled';
 import { PATH } from '@/constants';
+import Image from 'next/image';
+import { getCurrentDateTime } from '@/utils/getCurrentDateTime';
 
 interface ListProps {
   className?: string;
@@ -10,21 +12,45 @@ interface ListProps {
 
 interface ListItemProps {
   className?: string;
+  view: number;
   title: string;
+  endTime: string;
+  bookmark: number;
   recruitId: number;
+  companyName: string;
+  createdTime: string;
+  companyThumbnail: string;
 }
 
 export const List = ({ className, children }: ListProps) => {
   return <ul className={className}>{children}</ul>;
 };
 
-List.Item = function ListItem({ className, title, recruitId }: ListItemProps) {
+List.Item = function ListItem({
+  className,
+  recruitId,
+  title,
+  companyName,
+  companyThumbnail,
+  view,
+  bookmark,
+  createdTime,
+  endTime,
+}: ListItemProps) {
   return (
     <S.ListItem className={className}>
       <Link href={`${PATH.detail}/${recruitId}`} passHref>
         <S.AnchorWrapper>
-          <S.Title>{title}</S.Title>
-          <S.DescList>asd</S.DescList>
+          <Image src={companyThumbnail} alt="" width={80} height={80} />
+          <S.DescList>
+            <S.Title>{title}</S.Title>
+            <strong>{companyName}</strong>
+            <span>채용 시작일 {getCurrentDateTime(createdTime)}</span>
+            <br />
+            <span>채용 마감일 {getCurrentDateTime(endTime)}</span>
+            <span>{bookmark}</span>
+            <span>{view}</span>
+          </S.DescList>
         </S.AnchorWrapper>
       </Link>
     </S.ListItem>
--- src/components/domain/recruit/list/searchedList/SearchedList.component.tsx
+++ src/components/domain/recruit/list/searchedList/SearchedList.component.tsx
@@ -10,7 +10,17 @@ const SearchedList = ({ data }: SearchedListProps) => {
   return (
     <List>
       {data?.map((recruit) => (
-        <List.Item key={recruit.id} recruitId={recruit.id} title={recruit.title} />
+        <List.Item
+          key={recruit.id}
+          view={recruit.view}
+          title={recruit.title}
+          recruitId={recruit.id}
+          endTime={recruit.end_time}
+          bookmark={recruit.bookmark}
+          createdTime={recruit.created_time}
+          companyName={recruit.company.name}
+          companyThumbnail={recruit.company.logo_url}
+        />
       ))}
     </List>
   );
